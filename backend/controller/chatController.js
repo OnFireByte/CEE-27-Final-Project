3a5fef85782c3b5778335265bbd6e3444a158b8a
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2023-04-23 16:52:32 +0700

    feat: get message newer than timestamp

--- backend/controller/chatController.js
+++ backend/controller/chatController.js
@@ -29,6 +29,35 @@ export const getChat = async (req, res) => {
     }
 };
 
+export const getChatByTime = async (req, res) => {
+    const chat_id = req.params.chat_id;
+    const rawTimestamp = req.params.timestamp;
+    const timestamp = parseInt(rawTimestamp);
+    if (isNaN(timestamp)) {
+        res.status(400).send("Invalid timestamp");
+        return;
+    }
+    const params = {
+        TableName: "chat",
+        KeyConditionExpression: "#chat_id = :id AND #message_timestamp > :time",
+        ExpressionAttributeValues: {
+            ":id": chat_id,
+            ":time": timestamp,
+        },
+        ExpressionAttributeNames: {
+            "#message_timestamp": "timestamp",
+            "#chat_id": "chat_id",
+        },
+    };
+    try {
+        const data = await docClient.send(new QueryCommand(params));
+        res.send(data.Items);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+};
+
 export const addMessage = async (req, res) => {
     const created_date = Date.now();
     const user = req.user;
@@ -36,7 +65,8 @@ export const addMessage = async (req, res) => {
         name: `${user.firstname_en} ${user.lastname_en}`,
         user_id: user.uid,
         img: user.profile_pict,
-        ...req.body,
+        chat_id: req.body.chat_id,
+        message: req.body.message,
         timestamp: created_date,
     };
 
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -4,6 +4,7 @@ import auth from "../middleware/auth.js";
 const router = Router();
 
 router.get("/:chat_id", auth, chatController.getChat);
+router.get("/:chat_id/:timestamp", auth, chatController.getChatByTime);
 router.post("/", auth, chatController.addMessage);
 
 export default router;
